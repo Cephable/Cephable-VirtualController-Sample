b8bd8fa972fbb2f8b9607dd4e8b34025867d36fc
Alex Dunn <alexander.dunn@outlook.com>
2023-02-09 11:19:41 -0500

    Add sample project to receive device commands

---
+++ src/EnabledPlayVirtualControllerSample/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+Console.WriteLine("Enter your device token (you'll need to get this from the Enabled Play API on behalf of your user account at https://services.enabledplay.com/swagger): ");
+string deviceToken = Console.ReadLine();
+
+var connection = new HubConnectionBuilder()
+    .WithUrl("https://services.enabledplay.com/device", options =>
+    {
+        options.AccessTokenProvider = () => Task.FromResult(deviceToken);
+        options.Headers.Add("X-Device-Token", deviceToken);
+    })
+    .Build();
+
+connection.On<string>("DeviceCommand", (command) =>
+{
+    Console.WriteLine("Received command: " + command);
+});
+
+await connection.StartAsync();
+
+Console.WriteLine("Connected to hub. Waiting for commands... ");
+Console.WriteLine("Press enter to exit.");
+Console.ReadLine();
