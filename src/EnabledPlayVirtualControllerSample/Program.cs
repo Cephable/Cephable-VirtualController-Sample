3ad6a3cc8f6921334243bfbd02017ca90a004228
Alex Dunn <alexander.dunn@outlook.com>
2023-02-09 11:29:27 -0500

    update dependencies

--- src/EnabledPlayVirtualControllerSample/Program.cs
+++ src/EnabledPlayVirtualControllerSample/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.SignalR.Client;
+using Newtonsoft.Json;
 
 Console.WriteLine("Enter your device token (you'll need to get this from the Enabled Play API on behalf of your user account at https://services.enabledplay.com/swagger): ");
 string deviceToken = Console.ReadLine();
@@ -15,6 +16,7 @@ connection.On<string>("DeviceCommand", (command) =>
 {
     Console.WriteLine("Received command: " + command);
 });
+
 
 await connection.StartAsync();
 
