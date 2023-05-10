deccd51ea5eda67093731d96aceab456cd9cf006
Jon Campbell <joncamp@microsoft.com>
2023-05-10 13:02:04 -0700

    Fix dotnet code sample
    The dotnet code sample did not receive commands. The issue was the lack of completing the handshake via "VerifySelf". Added the missing command to the connection initialization sequence.

--- src/dotnet/EnabledPlayVirtualControllerSample/Program.cs
+++ src/dotnet/EnabledPlayVirtualControllerSample/Program.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.SignalR.Client;
-using Newtonsoft.Json;
 
 Console.WriteLine("Enter your device token (you'll need to get this from the Enabled Play API on behalf of your user account at https://services.enabledplay.com/swagger): ");
 string deviceToken = Console.ReadLine();
@@ -17,8 +16,10 @@ connection.On<string>("DeviceCommand", (command) =>
     Console.WriteLine("Received command: " + command);
 });
 
-
 await connection.StartAsync();
+
+// Connection started, indicate to hub that we are listening
+await connection.InvokeAsync("VerifySelf");
 
 Console.WriteLine("Connected to hub. Waiting for commands... ");
 Console.WriteLine("Press enter to exit.");
